ae2cb8078831059c410230775cd5405d6ed674d1
flozer :3 <161006821+flozerwithadot@users.noreply.github.com>
2024-04-21 12:03:17 +1200

    Create yippee.py

---
+++ yippee.py
@@ -0,0 +1,8 @@
+print("Welcome to Mad Libs!")
+name = input("What is your name? ")
+adj = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+place = input("Enter a place: ")
+food = input("Enter a food: ")
+vehicle = input("Enter a vehicle: ")
+print(f"{name} is a very {adj} person to be around. They love to cook in {place}. Their favourite food is {food} which they eat in a {vehicle}.")
